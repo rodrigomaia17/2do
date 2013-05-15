d1173e6da38b562f95c6746751f24910edd43010
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2013-05-15 06:27:04 -0300

    Fixed Rota Default

--- 2do/App_Start/RouteConfig.cs
+++ 2do/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace _2do
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Projeto", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
